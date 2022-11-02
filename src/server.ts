b7bb4b24541a6a8e06ee9c172cca4b227010bad3
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2022-11-02 11:25:38 +0100

    első

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,34 @@
-import * as express from 'express';
- 
+import * as express from "express";
+import * as morgan from "morgan";
+
+// function loggerMiddleware(
+//   request: express.Request,
+//   response: express.Response,
+//   next
+// ) {
+//   console.log(`${request.method} ${request.path}`);
+//   next();
+// }
+
+// app.use(loggerMiddleware);
+
 const app = express();
- 
-app.get('/', (request, response) => {
-  response.send('Hello world!');
+const router = express.Router();
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(router);
+
+router.get("/", (request, response) => {
+  response.send({
+    hostname: request.hostname,
+    path: request.path,
+    method: request.method,
+  });
 });
- 
+
+router.post("/hello", (request, response) => {
+  response.send("Hello World!");
+});
+
 app.listen(5000);
